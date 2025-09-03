823c9e25b9e085999076f6aef606c931bfedbdfb
Evan Alvarez <evanfalvarez@tutamail.com>
2025-09-02 21:50:24 -0500

    you can now parse the rss feed

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tokio;
 
+use crate::rss::{get_rss_feed, parse_rss_feed};
+
 mod config;
 mod rss;
 mod typst;
@@ -10,17 +12,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::get_config();
 
     for source in &config.news.sources {
-        match rss::get_rss_feed(&source.url).await {
+        match get_rss_feed(&source.url).await {
             Ok(channel) => {
                 println!("Fetched from: {} ({})", source.name, source.url);
                 println!("Feed title: {}", channel.title);
-                let data = typst::fill_newspaper_data(&channel.title, &channel.description)?;
-                println!("{:?}", data)
+
+                if let Err(e) = parse_rss_feed(&source.url).await {
+                    eprintln!("Error parsing RSS from {}: {}", source.name, e);
+                }
             }
             Err(e) => {
-                eprintln!("Error fetching RSS from {}: {}", source.name, e)
+                eprintln!("Error fetching RSS from {}: {}", source.name, e);
             }
-        };
+        }
     }
 
     Ok(())
--- src/rss.rs
+++ src/rss.rs
@@ -1,4 +1,5 @@
 use rss::Channel;
+use rss_gen::parse_rss;
 use std::error::Error;
 
 pub async fn get_rss_feed(url: &str) -> Result<Channel, Box<dyn Error>> {
@@ -6,4 +7,16 @@ pub async fn get_rss_feed(url: &str) -> Result<Channel, Box<dyn Error>> {
     let channel = Channel::read_from(&content[..])?;
 
     Ok(channel)
+}
+
+pub async fn parse_rss_feed(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let response = reqwest::get(url).await?;
+    let rss_content = response.text().await?;
+
+    match parse_rss(&rss_content, None) {
+        Ok(parsed_data) => println!("Parsed RSS data: {:?}", parsed_data),
+        Err(e) => eprintln!("There was an error parsing the RSS feed: {}", e),
+    }
+
+    Ok(())
 }
